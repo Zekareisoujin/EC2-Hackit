f06397d199d9042baac3c0a92ac04ca806b6de7a
Lolo-Ubuntu <lolo@ubuntu.(none)>
2012-12-11 15:47:36 +0800

    added authentication

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -51,6 +51,8 @@ Blog::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   resources :users do
+      match "login", :on => :collection
+      match "post_media", :on => :collection
   end
   root :to => 'home#index'
 
